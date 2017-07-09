041c3024d7ab59c6e6f9a3a17cdd2259bb15b485
Ibrahim Adam <PC@Ibrahims-Mac-mini.local>
2017-07-09 21:58:48 +0300

    Sign in ui custom classes complete

---
+++ DevslopsSocial/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  DevslopsSocial
+//
+//  Created by Ibrahim Adam on 10/15/1438 AH.
+//  Copyright © 1438 Ibrahim Adam. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
